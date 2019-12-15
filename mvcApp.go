59022288f62d7f85eebf233ad726308cf338c3ed
huzhouyu <a1085052074@qq.com>
2019-12-15 13:57:42 +0800

    更改示例

--- mvcApp.go
+++ mvcApp.go
@@ -44,7 +44,7 @@ func (app *MvcApp) AddHandlers(ic ...router.IController) {
 	}
 }
 
-// 主节点路由匹配原则注册     目前系统变量支持格式为 `/{ Controller}/{Action}/{id:int}/{who:string}`
+// 主节点路由匹配原则注册     目前系统变量支持格式为 `/{ Controller}/{Action}/{id:int}/{who:string}/{allString}`
 //
 // 如果不进行路由注册  默认为/{ Controller}/{Action}   router.ControllerActionInfo中 ControllerActionFuncName不用设置  设置了也不会生效
 func (app *MvcApp) AddMainRouter(list ...*router.ControllerActionInfo) {
