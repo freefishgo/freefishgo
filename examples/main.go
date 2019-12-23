52d2181a29d4acf4ce885a4f97b0d8e9c0465362
huzhouyu <a1085052074@qq.com>
2019-12-23 18:29:13 +0800

    修复路由重复

--- examples/main.go
+++ examples/main.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/freeFishGo/examples/controllers"
 	"github.com/freeFishGo/examples/fishgo"
 	_ "github.com/freeFishGo/examples/routers"
-	"github.com/freeFishGo/middlewares/httpToHttps"
 	"github.com/freeFishGo/middlewares/printTimeMiddleware"
 )
 
@@ -13,7 +12,7 @@ func main() {
 	// 通过注册中间件来打印任务处理时间服务
 	conf.Build.UseMiddleware(&printTimeMiddleware.PrintTimeMiddleware{})
 	// 利用中间件来实现http到https的转换
-	conf.Build.UseMiddleware(&httpToHttps.HttpToHttps{})
+	//conf.Build.UseMiddleware(&httpToHttps.HttpToHttps{})
 	// 把mvc实例注册到管道中
 	conf.Build.UseMiddleware(fishgo.Mvc)
 	conf.Build.Config.Listen.HTTPPort = 8080
