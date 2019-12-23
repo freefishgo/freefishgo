ab546e7c21fa1c95477f54729d8dc1b9a71cbc66
huzhouyu <huzhouyu@eatown.com.cn>
2019-12-23 16:32:07 +0800

    整理文档结构

---
+++ examples/conf/config.go
@@ -0,0 +1,27 @@
+package conf
+
+import (
+	"encoding/json"
+	"github.com/freeFishGo"
+	appConfig "github.com/freeFishGo/config"
+	"github.com/freeFishGo/examples/fishgo"
+	"github.com/freeFishGo/middlewares/mvc/router"
+	"os"
+)
+
+var Build *freeFishGo.ApplicationBuilder
+
+type config struct {
+	*appConfig.Config
+	WebConfig *router.WebConfig
+}
+
+func init() {
+	Build = freeFishGo.NewFreeFishApplicationBuilder()
+	conf := new(config)
+	f, _ := os.Open("conf/app.conf")
+	json.NewDecoder(f).Decode(conf)
+	Build.Config = conf.Config
+	fishgo.Mvc.Config = conf.WebConfig
+
+}
--- examples/main.go
+++ examples/main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/freeFishGo"
-	appConfig "github.com/freeFishGo/config"
+	"github.com/freeFishGo/examples/conf"
 	_ "github.com/freeFishGo/examples/controllers"
 	"github.com/freeFishGo/examples/fishgo"
 	_ "github.com/freeFishGo/examples/routers"
 	"github.com/freeFishGo/middlewares/httpToHttps"
-	"github.com/freeFishGo/middlewares/mvc/router"
 	"github.com/freeFishGo/middlewares/printTimeMiddleware"
-	"os"
 )
-
-var build *freeFishGo.ApplicationBuilder
-
-type config struct {
-	*appConfig.Config
-	WebConfig *router.WebConfig
-}
-
-func init() {
-	build = freeFishGo.NewFreeFishApplicationBuilder()
-	conf := new(config)
-	f, _ := os.Open("conf/app.conf")
-	json.NewDecoder(f).Decode(conf)
-	build.Config = conf.Config
-	fishgo.Mvc.Config = conf.WebConfig
-
-}
 
 func main() {
 	// 通过注册中间件来打印任务处理时间服务
-	build.UseMiddleware(&printTimeMiddleware.PrintTimeMiddleware{})
+	conf.Build.UseMiddleware(&printTimeMiddleware.PrintTimeMiddleware{})
 	// 利用中间件来实现http到https的转换
-	build.UseMiddleware(&httpToHttps.HttpToHttps{})
+	conf.Build.UseMiddleware(&httpToHttps.HttpToHttps{})
 	// 把mvc实例注册到管道中
-	build.UseMiddleware(fishgo.Mvc)
-	build.Config.Listen.HTTPPort = 8080
-	build.Run()
+	conf.Build.UseMiddleware(fishgo.Mvc)
+	conf.Build.Config.Listen.HTTPPort = 8080
+	conf.Build.Run()
 }
