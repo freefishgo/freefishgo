c441e91cff86944acbda8b8ec70defc0859d425e
huzhouyu <a1085052074@qq.com>
2020-01-10 22:18:44 +0800

    美好代码

--- examples/main.go
+++ examples/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/freefishgo/freefishgo"
 	_ "github.com/freefishgo/freefishgo/examples/conf"
 	_ "github.com/freefishgo/freefishgo/examples/routers"
 	"github.com/freefishgo/freefishgo/middlewares/mvc"
 	"github.com/freefishgo/freefishgo/middlewares/printTimeMiddleware"
+	"os"
 )
+type config struct {
+	*freefishgo.Config
+	WebConfig *mvc.MvcWebConfig
+}
+
+func init() {
+	conf := new(config)
+	f, err := os.Open("conf/app.conf")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = json.NewDecoder(f).Decode(conf)
+	if err != nil {
+		panic(err)
+	}
+	freefishgo.SetDefaultApplicationBuilderConfig(conf.Config)
+	mvc.SetDefaultMvcWebConfig(conf.WebConfig)
+
+}
 
 func main() {
 	// 通过注册中间件来打印任务处理时间服务
