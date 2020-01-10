b6dde356be65d968dc7a32df6e5136dc3e2e6cb8
huzhouyu <a1085052074@qq.com>
2020-01-10 20:15:33 +0800

    修复linux文件大小写问题
    Signed-off-by: huzhouyu <a1085052074@qq.com>

---
+++ middlewares/mvc/controller_test.go
@@ -0,0 +1,24 @@
+package mvc
+
+import (
+	"testing"
+)
+
+func TestReplaceActionNameIgone(T *testing.T) {
+	t := []struct {
+		f string
+		r string
+	}{
+		{f: "nihao", r: "nihao"},
+		{f: "nihao", r: "nihao"},
+		{f: "niHao", r: "niHao"},
+		{f: "nihaoget", r: "nihao"},
+		{f: "nihaoGet", r: "nihao"},
+		{f: "nIhaoGeT", r: "nIhao"},
+	}
+	for _, v := range t {
+		if replaceActionNameIgone(v.f) != v.r {
+			T.Errorf("测试失败%+v,因为%s不等于%s", v, replaceActionNameIgone(v.f), v.r)
+		}
+	}
+}
