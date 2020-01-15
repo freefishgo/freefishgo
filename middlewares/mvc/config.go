7bbabb95d8f7bb9293ec66ad272f9a19ff752ca5
huzhouyu <huzhouyu@eatown.com.cn>
2020-01-15 13:30:14 +0800

    mvc默认静态文件在 根目录static下
    Signed-off-by: huzhouyu <huzhouyu@eatown.com.cn>

--- middlewares/mvc/config.go
+++ middlewares/mvc/config.go
@@ -35,5 +35,5 @@ type MvcWebConfig struct {
 
 // 实例化一个 MvcWebConfig
 func NewWebConfig() *MvcWebConfig {
-	return &MvcWebConfig{ViewsPath: "", TemplateLeft: "{{", TemplateRight: "}}", IsDevelopment: false, LayoutTemplateLeft: "{{{", LayoutTemplateRight: "}}}"}
+	return &MvcWebConfig{ViewsPath: "static", TemplateLeft: "{{", TemplateRight: "}}", IsDevelopment: false, LayoutTemplateLeft: "{{{", LayoutTemplateRight: "}}}"}
 }
