d6bc5873f353c92da3cf6c302ed6e1e2e284a3c7
freefishgo <a1085052074@qq.com>
2020-04-09 12:28:38 +0800

    修复继承类控制器参数不能注入继承类的参数

--- middlewares/mvc/reqStringToStruct.go
+++ middlewares/mvc/reqStringToStruct.go
@@ -20,14 +20,21 @@ func MapStringToStruct(i interface{}, data map[string]interface{}) interface{} {
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
 		name := f.Tag.Get("json")
+		v1 := v.Field(i)
 		if name == "" {
 			name = f.Name
+			_, ok := data[name]
+			if !ok {
+				if v1.Kind() == reflect.Struct || (v1.Kind() == reflect.Ptr && v1.Elem().Kind() == reflect.Struct) {
+					MapStringToStructInReflect(v1, data)
+				}
+				continue
+			}
 		}
 		val, ok := data[name]
 		if !ok {
 			continue
 		}
-		v1 := v.Field(i)
 		switch v1.Kind() {
 		case reflect.Slice:
 			if val, ok := val.([]string); ok {
@@ -57,31 +64,95 @@ func MapStringToStruct(i interface{}, data map[string]interface{}) interface{} {
 	return i
 }
 
+func MapStringToStructInReflect(v reflect.Value, data map[string]interface{}) interface{} {
+	t := v.Type()
+	if t.Kind() == reflect.Ptr {
+		if !v.Elem().CanSet() {
+			v.Set(reflect.New(t.Elem()))
+		}
+		v = v.Elem()
+		t = t.Elem()
+	}
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		name := f.Tag.Get("json")
+		v1 := v.Field(i)
+		if name == "" {
+			name = f.Name
+			_, ok := data[name]
+			if !ok {
+				if v1.Kind() == reflect.Struct || (v1.Kind() == reflect.Ptr && v1.Elem().Kind() == reflect.Struct) {
+					MapStringToStructInReflect(v1, data)
+				}
+				continue
+			}
+		}
+		val, ok := data[name]
+		if !ok {
+			continue
+		}
+		switch v1.Kind() {
+		case reflect.Slice:
+			if val, ok := val.([]string); ok {
+				sl := reflect.MakeSlice(v1.Type(), len(val), len(val))
+				b := false
+				for i := 0; i < len(val); i++ {
+					if doBasic(sl.Index(i), f.Type.Elem(), val[i]) {
+						b = true
+					}
+				}
+				if b {
+					v1.Set(sl)
+				}
+			}
+			continue
+		case reflect.Ptr:
+			if !v1.Elem().CanSet() {
+				v1.Set(reflect.New(f.Type.Elem()))
+			}
+			doBasic(v1.Elem(), f.Type.Elem(), val)
+			continue
+		default:
+			doBasic(v1, f.Type, val)
+			continue
+		}
+	}
+	return v
+}
+
 func doBasic(v1 reflect.Value, t1 reflect.Type, val interface{}) bool {
 	switch t1.Kind() {
 	case reflect.String:
 		if val, ok := val.(string); ok {
-			v1.SetString(val)
+			if v1.CanSet() {
+				v1.SetString(val)
+			}
 		}
 		break
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		if val, ok := val.(string); ok {
 			if val, err := strconv.ParseInt(val, 10, 64); err == nil {
-				v1.SetInt(val)
+				if v1.CanSet() {
+					v1.SetInt(val)
+				}
 			}
 		}
 		break
 	case reflect.Bool:
 		if val, ok := val.(string); ok {
 			if val, err := strconv.ParseBool(val); err == nil {
-				v1.SetBool(val)
+				if v1.CanSet() {
+					v1.SetBool(val)
+				}
 			}
 		}
 		break
 	case reflect.Float32, reflect.Float64:
 		if val, ok := val.(string); ok {
 			if val, err := strconv.ParseFloat(val, 64); err == nil {
-				v1.SetFloat(val)
+				if v1.CanSet() {
+					v1.SetFloat(val)
+				}
 			}
 		}
 		break
