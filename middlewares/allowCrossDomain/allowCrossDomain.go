76515a1df66714f4a2a0d9200a185fc4879e5e43
胡周瑜 <huzhouyu@eatown.com.cn>
2020-06-06 15:49:53 +0800

    增加跨域修改请求头

--- middlewares/allowCrossDomain/allowCrossDomain.go
+++ middlewares/allowCrossDomain/allowCrossDomain.go
@@ -24,12 +24,15 @@ type AllowCrossDomain struct {
 	AllowOrigin string
 	// 允许跨域方法 格式 GET, POST, DELETE,PUT
 	AllowMethods string
+	// 允许跨域请求头
+	AllowHeaders string
 }
 
 // 中间件实现允许跨域请求
 func (allow *AllowCrossDomain) Middleware(ctx *freefishgo.HttpContext, next freefishgo.Next) *freefishgo.HttpContext {
 	ctx.Response.Header().Set("Access-Control-Allow-Origin", allow.AllowOrigin)
 	ctx.Response.Header().Set("Access-Control-Allow-Methods", allow.AllowMethods)
+	ctx.Response.Header().Set("Access-Control-Allow-Headers", allow.AllowHeaders)
 	if http.MethodOptions == ctx.Request.Method {
 		return ctx
 	}
@@ -43,5 +46,8 @@ func (allow *AllowCrossDomain) LastInit(config *freefishgo.Config) {
 	}
 	if allow.AllowMethods == "" {
 		allow.AllowMethods = "GET, POST, DELETE,PUT"
+	}
+	if allow.AllowHeaders == "" {
+		allow.AllowHeaders = "*"
 	}
 }
